ca5abe1fbdeba7c1719ecaffbd0e16f46dba1c92
Vincent Edwards <mister.man489@gmail.com>
2025-06-01 20:08:25 -0700

    Create an encoding list (digit -> frequencies)

--- scripts/DTMFfrequencies.py
+++ scripts/DTMFfrequencies.py
@@ -2,9 +2,19 @@
 
 low = [697, 770, 852, 941]
 high = [1209, 1336, 1477]
+
 decode_matrix = [
     [ 1, 2, 3],
     [ 4, 5, 6],
     [ 7, 8, 9],
     [-1, 0,-1],
 ]
+
+encode_list = [(-1, -1)] * 10
+for i, row in enumerate(decode_matrix):
+    low_freq = low[i]
+    for j, digit in enumerate(row):
+        high_freq = high[j]
+        if digit not in range(10):
+            continue
+        encode_list[digit] = (low_freq, high_freq)
